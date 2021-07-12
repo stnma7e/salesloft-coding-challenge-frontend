3ab2bd2a789e84be5b3571d2f9bea8fe26fbdc2d
Sam Delmerico <svdelmerico@gmail.com>
2021-07-11 21:38:03 -0400

    add unit test to check FrequencyChart only shows characters with frequency > 0

---
+++ src/components/FrequencyChart.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FrequencyChart } from './FrequencyChart';
+
+
+test('frequency chart only contains letters that are given', () => {
+    const inputs: string[] = [
+        "asdf",
+    ];
+    render(<FrequencyChart inputs={inputs} />);
+    
+    for (let input of inputs) {
+        for (let c of "abcdefghijklnmopqrstuvwxyz") {
+            if (input.indexOf(c) > -1) {
+                const label = screen.getByText(c + ": 1");
+                expect(label).toBeInTheDocument();
+            }
+        }
+    }     
+});
